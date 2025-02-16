d4f8d8a79d94de3852effe87f4877c6300f46fd0
DINUWH-MDfake <dinuwahmd@gmail.com>
2025-02-16 11:24:34 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94728899640"
+const SESSION_ID = "PRABATH-MD~rXRnBCaK#e21s4Z9hTj4lYDAcmUBNOpMhoxTfWB-iHIKjb_66Z1Q"
 
 
 module.exports = {
